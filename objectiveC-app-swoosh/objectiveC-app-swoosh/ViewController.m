02742d1d90ca64bdb7a6ec1c773fa37cf65e9d2d
Rakesh Gorkhanath Khandagale <khandagale87.rakesh@gmail.com>
2021-02-19 15:35:14 -0500

    objective-c initial xcode project setup

---
+++ objectiveC-app-swoosh/objectiveC-app-swoosh/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  objectiveC-app-swoosh
+//
+//  Created by Rakesh Khandagale on 2/19/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
