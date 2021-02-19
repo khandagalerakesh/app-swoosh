9a2f625011e13b7c365a192cbfbe185b89e9c14a
Rakesh Gorkhanath Khandagale <khandagale87.rakesh@gmail.com>
2021-02-19 15:31:05 -0500

    inital swiftui xcode project setup

---
+++ swiftui-app-swoosh/swiftui-app-swoosh/swiftui_app_swooshApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_app_swooshApp.swift
+//  swiftui-app-swoosh
+//
+//  Created by Rakesh Khandagale on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_app_swooshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
