b040b5487fceac6ea469fa73b78b4f6b42fad0ab
Rakesh Gorkhanath Khandagale <khandagale87.rakesh@gmail.com>
2021-02-19 15:22:29 -0500

    initial swift xcode project setup

---
+++ swift-app-swoosh/swift-app-swoosh/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  swift-app-swoosh
+//
+//  Created by Rakesh Khandagale on 2/19/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK:- Properties
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
